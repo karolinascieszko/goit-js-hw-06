f47fdc5dca687d2e0ca67a89314f1bbfbc5b89dc
Karolina Ścieszko <karolinascieszko@gmail.com>
2021-08-23 20:51:35 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -4,7 +4,7 @@ const qsa = (selector) => document.querySelectorAll(selector);
 
 const items = qsa("li.item");
 
-console.log(`Numbers of categories: ${items.length}`);
+console.log(`Number of categories: ${items.length}`);
     [...items].forEach((item) => {
     console.log(`Category: ${item.children[0].textContent}`);
     console.log(`Elements: ${item.children[1].children.length}`);
